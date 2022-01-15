f78cdd906d5cbcea4677699775fc1b3c106a4a14
Jadiael Monteiro <jadiael.monteiro@gmail.com>
2022-01-15 08:47:29 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import Navbar from "./components/Navbar";
 import './components/Navbar/styles.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
-  return (    
-    <Navbar />
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
