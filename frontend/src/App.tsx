c204b6ff4ad8aff60c4f3f72d81ada9d55dc1549
Jadiael Monteiro <jadiael.monteiro@gmail.com>
2022-01-14 22:06:25 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+import './components/Navbar/styles.css';
+
 function App() {
-  return (
-    <h1>Pagina principal</h1>
+  return (    
+    <Navbar />
   );
 }
 
